bae1266f0347514fca329bd7fbe9d3aea35c22c5
Andrew Straw <strawman@astraw.com>
2014-02-25 23:43:47 +0100

    update for flymad

--- scripts/electric_check_flymad.bash
+++ scripts/electric_check_flymad.bash
@@ -2,18 +2,18 @@
 set -e
 
 if [ ! "$ROS_TARGET" ] ; then export ROS_TARGET=$HOME/ros.electric.boost1.46 ; fi
-if [ ! "$FLYVR_TARGET" ] ; then export FLYVR_TARGET=$HOME/ros-flyvr.electric.boost1.46 ; fi
+if [ ! "$FLYMAD_TARGET" ] ; then export FLYMAD_TARGET=$HOME/ros-flymad.electric.boost1.46 ; fi
 
-rosinstall --nobuild $FLYVR_TARGET $ROS_TARGET https://raw.github.com/strawlab/rosinstall/master/strawlab-electric-flyvr.rosinstall
+rosinstall --nobuild $FLYMAD_TARGET $ROS_TARGET https://raw.github.com/strawlab/rosinstall/master/strawlab-electric-flymad.rosinstall
 
-source $FLYVR_TARGET/setup.bash
-cd $FLYVR_TARGET
+source $FLYMAD_TARGET/setup.bash
+cd $FLYMAD_TARGET
 
 # Now parse our rosdeps and tell ubuntu to install all the packages.
 wget https://raw.github.com/strawlab/rosinstall/master/scripts/parse_rosdep -O parse_rosdep
 chmod a+x parse_rosdep
 
-STACKS="joystick_drivers flyvr browser_joystick rosgobject"
+STACKS="flymad joystick_drivers browser_joystick rosgobject"
 
 # Figure out what packages we need to apt-get and install them -----------------
 rosdep generate_bash $STACKS > rosdep.output || true
